e2e9f1a62022f107f1a0e3b12082db34f5650933
JERRY <hwjameshw@gmail.com>
2023-07-04 10:58:07 +0900

    chore: flavor, 인증서 세팅

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,24 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        
+        let controller = window.rootViewController as! FlutterViewController
+        
+        let flavorChannel = FlutterMethodChannel(
+            name: "flavor",
+            binaryMessenger: controller.binaryMessenger)
+        
+        flavorChannel.setMethodCallHandler({(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            let flavor = Bundle.main.infoDictionary?["App Flavor"]
+            result(flavor)
+        })
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
